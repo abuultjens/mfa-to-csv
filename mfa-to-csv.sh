d80d66de6d1d444cb4f92802b3e98671a8f1cac1
Andrew Buultjens <buultjensa@gmail.com>
2020-01-14 17:22:12 +1100

    Add files via upload

--- mfa-to-csv.sh
+++ mfa-to-csv.sh
@@ -12,6 +12,7 @@
 
 #----------------------------------------------------
 
+# assign input and output file names
 ALIGNMENT=${1}
 OUTFILE=${3}
 
@@ -25,29 +26,37 @@ RAND=`echo "${RAND_1}${RAND_2}${RAND_3}"`
 
 #----------------------------------------------------
 
+# index input mfa
 samtools faidx ${ALIGNMENT}
+
+# write first line of index column
 echo "INDEX" > ${RAND}_fofn.txt
 
 #----------------------------------------------------
 
+# check if outfile already exists
 if ls ${OUTFILE} 1> /dev/null 2>&1; then
 	rm ${OUTFILE}
 fi
 
 #----------------------------------------------------
 
+# loop through all names in fofn
 for TAXA in $(cat $2); do
 
-
+	# write fa seq to file
 	samtools faidx ${ALIGNMENT} ${TAXA} | grep -v ">" | tr -d '\n' >> ${RAND}_tmp1.txt
+	# write end of line chr
 	echo '' >> ${RAND}_tmp1.txt
 
+	# write names to index column
 	echo "${TAXA}" >> ${RAND}_fofn.txt
 
 done
 
 #----------------------------------------------------
 
+# replace '-' chr and make into csv
 tr '-' 'N' < ${RAND}_tmp1.txt | sed 's/./&,/g' > ${RAND}_tmp2.txt
 
 #----------------------------------------------------
@@ -57,16 +66,21 @@ LEN=`head -1 ${RAND}_tmp2.txt | tail -1 | tr ',' '\n' | wc -l | awk '{print $1}'
 ONE=1
 let COUNT=${LEN}-${ONE}
 
+# make header
 seq 1 ${COUNT} | tr '\n' ',' > ${RAND}_seq_1-${LEN}.tr.csv
 echo '' >> ${RAND}_seq_1-${LEN}.tr.csv
 
 #----------------------------------------------------
 
+# cat header to csv
 cat ${RAND}_seq_1-${LEN}.tr.csv ${RAND}_tmp2.txt | sed 's/.$//' > ${RAND}_tmp3.txt
+
+# paste in index
 paste ${RAND}_fofn.txt ${RAND}_tmp3.txt | tr '\t' ',' > ${OUTFILE}
 
 #----------------------------------------------------
 
+# remove tmp files
 rm ${RAND}_fofn.txt
 rm ${RAND}_tmp1.txt
 rm ${RAND}_tmp2.txt
